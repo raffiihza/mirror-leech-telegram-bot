0648e61ad43bcae1d7096cbc171c21d81a25e052
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2023-10-26 17:42:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
+if [ -n "$TERABOX_COOKIES" ]; then
+  echo "$TERABOX_COOKIES" | base64 --decode > terabox.txt
+fi
+if [ -n "$RCLONE_CONFIG" ]; then
+  echo "$RCLONE_CONFIG" | base64 --decode > rclone.conf
+fi
+if [ -n "$TOKEN_PICKLE_URL" ]; then
+  curl -o token.pickle "$TOKEN_PICKLE_URL"
+fi
+if [ -n "$ACCOUNTS_ZIP_URL" ]; then
+  curl -o accounts.zip "$ACCOUNTS_ZIP_URL"
+fi
+
 python3 update.py && python3 -m bot
