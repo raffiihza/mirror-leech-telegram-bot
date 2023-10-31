33bf41120dc9163f4d1fbce09fa32f24bf681ae6
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2023-11-01 04:39:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 if [ -n "$TERABOX_COOKIES" ]; then
   echo "$TERABOX_COOKIES" | base64 --decode > terabox.txt
 fi
-if [ -n "$RCLONE_CONFIG" ]; then
-  echo "$RCLONE_CONFIG" | base64 --decode > rclone.conf
-fi
 
 python3 update.py && python3 -m bot
