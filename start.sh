fc5fca1be707d695518b686afeae86581255c72d
Raffi Ihza Zuhairnawan <115229257+raffiihza@users.noreply.github.com>
2023-11-23 11:51:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [ -n "$DIRECTLINK" ]; then
   rm bot/helper/mirror_utils/download_utils/direct_link_generator.py
-  curl -o bot/helper/mirror_utils/download_utils/direct_link_generator.py "$DIRECTLINK"
+  echo "$DIRECTLINK" | base64 --decode > bot/helper/mirror_utils/download_utils/direct_link_generator.py
 fi
 
 python3 update.py && python3 -m bot
